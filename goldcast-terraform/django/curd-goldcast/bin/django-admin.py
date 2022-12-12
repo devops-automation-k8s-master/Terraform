8ccc0d688761256e4f61aad1b8b8941ca4582240
devops <iitkgp.anavarkhan.devops@gmail.com>
2022-12-12 21:25:14 +0530

    refactor

--- goldcast-terraform/django/curd-goldcast/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/anwarkhan/project/goldcast-terraform/django/curd-goldcast/bin/python3.10
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
